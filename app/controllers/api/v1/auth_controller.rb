adbc9c89ebf72236a245271f2002a5fbb95404f1
github username <github email address>
2018-10-19 17:26:39 -0500

    backend auth working

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::AuthController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
   def create
-
     @user = User.find_by(username: user_login_params[:username])
     #User#authenticate comes from BCrypt
     if @user && @user.authenticate(user_login_params[:password])
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:index, :update, :create]
       post '/login' , to: 'auth#create'
+      post '/register', to: 'users#create'
       get '/profile', to: 'users#profile'
     end
   end
